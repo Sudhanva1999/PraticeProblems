f3fa5883b191cb5884eb767eb162dcf15f27d6f7
Sudhanva1999 <49947142+Sudhanva1999@users.noreply.github.com>
2022-10-08 21:40:42 +0530

    Adding O(n) solution

---
+++ InterviewBit/TrailingZeros.py
@@ -0,0 +1,11 @@
+def solve(A):
+        count = 0 
+        flag = 0 
+        while(flag == 0):
+            if((A & 1) == 0):
+                count += 1
+                A = A >> 1
+            else:
+                flag = 1 
+        return count 
+print(solve(int(input("Enter a integer to find number of trailing zeros it has in binary form\n"))))
